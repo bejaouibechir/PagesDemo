431e9f643af78200b7d7225392d78da6491fd405
bejaouibechir <me780411@gmail.com>
2024-07-24 09:26:33 +0200

    ajout dynamique des elements en flyout

--- PagesDemo/AppShell.xaml.cs
+++ PagesDemo/AppShell.xaml.cs
@@ -13,4 +13,35 @@ public partial class AppShell : Shell
         Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
 #endif    
     }
+
+
+    public void AddFlyoutItem(string title, string icon, Type pageType)
+    {
+        var flyoutItem = new FlyoutItem
+        {
+            Title = title,
+            Icon = icon
+        };
+
+        var tab = new Tab();
+        var shellContent = new ShellContent
+        {
+            ContentTemplate = new DataTemplate(pageType)
+        };
+        tab.Items.Add(shellContent);
+        flyoutItem.Items.Add(tab);
+
+        Items.Add(flyoutItem);
+    }
+
+    public void RemoveFlyoutItem(string title)
+    {
+        var item = Items.FirstOrDefault(i => i.Title == title);
+        if (item != null)
+        {
+            Items.Remove(item);
+        }
+    }
+
+
 }
--- PagesDemo/Pages/HomePage.xaml.cs
+++ PagesDemo/Pages/HomePage.xaml.cs
@@ -6,4 +6,43 @@ public partial class HomePage : ContentPage
 	{
 		InitializeComponent();
 	}
+    int index=0;
+    
+    private void AddFlyoutItem_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Ajout d'utilisateur", "Un nouveau utilisateur ajouté à l'espace", "OK");
+        (App.Current.MainPage as AppShell)
+			.AddFlyoutItem($"User {index++}","user.png",typeof(CustomPage));
+    }
+
+    private void RemoveFlyoutItem_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Ajout d'utilisateur", "Un nouveau utilisateur supprimé de l'espace", "OK");
+        (App.Current.MainPage as AppShell)
+            .RemoveFlyoutItem($"User {index}");
+    }
+}
+
+public class CustomPage:ContentPage
+{
+    Label content;
+    static int index = 0;
+    
+    public CustomPage(int index)
+    {
+
+    }
+    
+    public CustomPage()
+    {
+        content = new Label
+        {
+            Text = $"Espace réservé au user {new Random().Next(1,10000)}"
+        };
+        content.VerticalOptions= LayoutOptions.CenterAndExpand;
+        content.HorizontalOptions = LayoutOptions.CenterAndExpand;
+        content.Margin = new Thickness(20);
+        content.FontSize = 25;
+        Content = content;  
+    }
 }
